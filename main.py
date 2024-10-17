457e51704fb04b04b97608e14cd5de30e7fdc855
= <=>
2024-10-17 07:13:50 +0530

    add frontend domain

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with specific origins as needed
+    allow_origins=["http://localhost:3000", "https://vectorshift-navy.vercel.app"],  # Replace with specific origins as needed
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
